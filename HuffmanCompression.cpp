095e5470f5746668726d5aa631421e0ea17eeef0
konFMI <gikolev@abv.bg>
2021-01-10 18:04:51 +0200

    Prelease version 	- The code is in one file. 	- There are debug functions and features with macros. 	- The code is straightforward. 	- There have been developed test scrips for -c and -d. 	- The code works with chars only. A.k.a uint8_t. 	- The compression is not very well. 	- Maybe the histogram being in the output file is too much?

--- HuffmanCompression.cpp
+++ HuffmanCompression.cpp
@@ -6,26 +6,143 @@
 #include <queue>
 #include <vector>
 
-#define SIZE 32
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define DEBUG 0
+#define PRINT_STARS()(std:: cout << "\n*****************************************\n")
+
+static void BinaryPrint(
+	UINTBITMAP num)
+{
+	UINTBITMAP mask = 1 << BITMAPENTRYBITS - 1;
+
+	while (mask)
+	{
+		std::cout << ((num & mask) != 0);
+		mask >>= 1;
+	}
+	
+	std::cout << "\n";
+}
+
+static void PrintBinaryBuffer(
+	UINTBITMAP list[],
+	UINTBITMAP index)
+{
+	std::cout << "\nindex =\t" << (int)index << "\n";
+	for (int i = 0; i < index / BITMAPENTRYBITS && i < SIZE; i++)
+	{
+		std::cout << list[i] << "\t";
+		BinaryPrint(list[i]);
+	}
+
+}
+
+static void PrintCodeTable(
+	std::map<char, std::string> huffmanCodeMap)
+{
+	for (auto pair : huffmanCodeMap)
+	{
+		std::cout << pair.first << "\t" << pair.second << std::endl;
+	}
+
+	PRINT_STARS();
+}
+
+static void PrintHistogram(
+	std::map<char, UINTHISTVALUE> histogram)
+{
+	for (auto pair : histogram)
+	{
+		if (pair.second)
+		{
+			std::cout << pair.first << "\t" << pair.second << std::endl;
+		}
+	}
+}
 
 static void PrintInputStreamStateMessage(
 	std::ifstream &stream)
 {
 	std::cout << "Input stream is " << ((stream.good())? "good" : "bad") << std::endl;
 }
-static void PrintInputErrorMessage()
-{
-	std::cerr << "error:\nusage:\tcompressSoft -[c/d] -[i/o] fileName -[i/o] fileName\n";
-	std::cerr << "c : compress\td : decmpress\ni : input\to : output\n";
-}
 
-static void PrintOpenFileErrorMessage(
+static void ErrorOpenFile(
 	std::string pathToInputFile)
 {
-	std::cerr << "failed to open file '" << pathToInputFile << "'\n";
+	std::cerr << "error: failed to open file '" << pathToInputFile << "'\n";
+	exit(1);
 }
 
-static void ClearBuffer(uint32_t buffer[], int count)
+static void ErrorInvalidOption(
+)
+{
+	std::cerr << "error: Invalid option. Only -c or -d.";
+	exit(1);
+}
+
+static void ErrorInvalidInputFormat(
+)
+{
+	std::cerr << "error: Invalid input format.\nusage:\tcompressSoft -[c/d] -[i/o] fileName -[i/o] fileName\n";
+	std::cerr << "c : compress\td : decmpress\ni : input\to : output\n";
+	exit(1);
+}
+
+static void HistogramEmptyCheck(
+	std::map<char, UINTHISTVALUE>& histogram)
+{
+	if (histogram.empty())
+	{
+		std::cerr << "error:\tNo histogram available. Please check your input file." << std::endl;
+		exit(1);
+	}
+}
+
+static int BufferFull(
+	UINTBITMAP list[],
+	UINTBITMAP& index)
+{
+	if (index >= (SIZE * BITMAPENTRYBITS))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+static int UpdateBitInBuffer(
+	UINTBITMAP list[],
+	UINTBITMAP& index,
+	UINTBITMAP bitValue)
+{
+	if (index < 0 || index >= SIZE * BITMAPENTRYBITS)
+	{
+		return 1;
+	}
+
+
+	int minorIndex = index % BITMAPENTRYBITS;
+	int mainIndex = index / BITMAPENTRYBITS;
+
+	if (!!bitValue)
+	{
+		list[mainIndex] = list[mainIndex] | (1 << (BITMAPENTRYBITS - minorIndex - 1));
+	}
+	else
+	{
+		list[mainIndex] = list[mainIndex] & ~(1 << (BITMAPENTRYBITS - minorIndex - 1));
+	}
+
+	index++;
+	return 0;
+}
+
+static void ClearBuffer(
+	UINTBITMAP buffer[],
+	int count)
 {
 	for (size_t i = 0; i < count; i++)
 	{
@@ -34,143 +151,61 @@ static void ClearBuffer(uint32_t buffer[], int count)
 	
 }
 
-static void BinaryPrint(uint32_t num)
-{
-	uint32_t mask = 1 << 31;
-
-	while (mask)
-	{
-		std::cout << ((num & mask) != 0);
-		mask >>= 1;
-	}
-	std::cout << std::endl;
-}
-
-static void PrintBinaryBuffer(uint32_t list[], int index)
-{
-	std::cout << "PRINT START" << std::endl;
-	std::cout << "index : " << index << std::endl;
-	for (int i = 0; i <= index / 32 && i < SIZE; i++)
-	{
-		std::cout << list[i] << "\t";
-		BinaryPrint(list[i]);
-	}
-	std::cout << "PRINT END" << std::endl;
-}
-
-static void PrintCodeTable(std::map<char, std::string> huffmanCodeMap)
-{
-	for (auto pair : huffmanCodeMap)
-	{
-		std::cout << pair.first << "\t" << pair.second << std::endl;
-	}
-}
-
-static int BufferFull(uint32_t list[], int& index)
-{
-	if (index >= (SIZE * 32))
-	{
-		return 1;
-	}
-	return 0;
-}
-
-static int UpdateBitInBuffer(uint32_t list[], int& index, uint8_t bitValue)
-{
-	int bits = 32;
-	if (index < 0 || index >= SIZE * bits)
-	{
-		return 1;
-	}
-
-
-	int minorIndex = index % bits;
-	int mainIndex = index / bits;
-
-	if (!!bitValue)
-	{
-		list[mainIndex] = list[mainIndex] | (1 << (bits - minorIndex - 1));
-	}
-	else
-	{
-		list[mainIndex] = list[mainIndex] & ~(1 << (bits - minorIndex - 1));
-	}
-
-	index++;
-	return 0;
-}
-
-HuffmanCompression::HuffmanCompression()
+HuffmanCompression::HuffmanCompression(
+)
 {
 	root = nullptr;
+	inputFd	 = -1;
+	outputFd = -1;
 }
 
-HuffmanCompression::~HuffmanCompression()
+HuffmanCompression::~HuffmanCompression(
+)
 {
 	inStream.close();
 	outStream.close();
 
+	close(inputFd);
+	close(outputFd);
+
 	DeleteTree(root);
 }
 
-int HuffmanCompression::Run(int argc, char *argv[])
+void HuffmanCompression::Run(
+	int argc,
+	char *argv[])
 {
-    if (ProcessInput(argc, argv))
-	{
-		PrintInputErrorMessage();
-		return 1;
-	}
+   	ProcessInput(argc, argv);
+	OpenWorkFiles();
 
-	this->inStream.open(pathToInputFile, std::ifstream::in);
-	if (!inStream.is_open())
-	{
-		PrintOpenFileErrorMessage(pathToInputFile);
-		return 1;
-	}
-
-	this->outStream.open(pathToOutputFile, std::ofstream::out | std::ofstream::trunc);
-	if (!outStream.is_open())
-	{
-		PrintOpenFileErrorMessage(pathToOutputFile);
-		return 1;
-	}
-
-	if (GenerateHistogram())
-	{
-		return 1;
-	}
-
+	GenerateHistogram();
 	BuildHuffmanTree();
 	GenerateHuffmanCodeMap(root, "");
-
+	
 	// By this point everything is set. We can compess or decompress now.
 	if (option == 'c')
 	{
-		WriteHistogram();
 		EncodeStream();
 	}
-	else if (option == 'd')
+	else
 	{
-
+		DecodeStream();
 	}
-
-    return 0;
 }
 
-/* Check if the input arguments are in the expected format. */
-int HuffmanCompression::ProcessInput(
+void HuffmanCompression::ProcessInput(
 	int argc,
 	char *argv[])
 {
 	/* Check argument length. */
-	if (argc != 6) return 1;
-
+	
 	/* Check individual arguments lenght. */
-	if (strlen(argv[1]) != 2 ||
+	if (argc 			!= 6 ||
+		strlen(argv[1]) != 2 ||
 	    strlen(argv[2]) != 2 ||
 	    strlen(argv[4]) != 2)
 	 {
-	 	return 1;
+	 	ErrorInvalidInputFormat();
 	 }
 
 	/* First option for -[c/d] */
@@ -178,7 +213,7 @@ int HuffmanCompression::ProcessInput(
 	   (argv[1][1] != 'c' &&
 	    argv[1][1] != 'd'))
 	 {
-	 	return 1;
+		 ErrorInvalidOption();
 	 }
 
 	/* Second and third option for -[i/o] */
@@ -192,112 +227,67 @@ int HuffmanCompression::ProcessInput(
 	   (argv[2][1] == 'o'  &&
 	    argv[4][1] == 'o'))
 	 {
-		return 1;
+		ErrorInvalidInputFormat();
 	 }
 
 	int indexToInputFile  = (argv[2][1] == 'i')? 3 : 5;
 	int indexToOutputFile = (argv[2][1] == 'o')? 3 : 5;
 
-
 	this->option = argv[1][1];
 	this->pathToInputFile = argv[indexToInputFile];
 	this->pathToOutputFile = argv[indexToOutputFile];
-
-	return 0;
 }
 
-int HuffmanCompression::GenerateHistogram()
+void HuffmanCompression::OpenWorkFiles()
 {
-	std::string line;
+	this->inStream.open(pathToInputFile, std::ifstream::in);
+	if (!inStream.is_open())
+	{
+		ErrorOpenFile(pathToInputFile);
+	}
+
+	inputFd = open( pathToInputFile.c_str(), O_RDONLY);
+	if (inputFd == -1)
+	{
+		ErrorOpenFile(pathToInputFile);
+	}
+
+	outputFd = open( pathToOutputFile.c_str(), O_WRONLY);
+	if (outputFd == -1)
+	{
+		ErrorOpenFile(pathToOutputFile);
+	}
+	
+
+	this->outStream.open(pathToOutputFile, std::ofstream::out | std::ofstream::trunc);
+	if (!outStream.is_open())
+	{
+		ErrorOpenFile(pathToOutputFile);
+	}
+}
+
+void HuffmanCompression::GenerateHistogram(
+)
+{
 	//TODO: Depending on the option value, the histogram will be generated
 	//		by a text for encoding or a from an encoded file.
 	//		In one way it is straightforward. The other needs more attention.
-
-	//Straightforward way
+	//DONE!
 	switch (option)
 	{
 	case 'c':
-		while (inStream.good())
-		{
-			std::getline(inStream, line);
-			for (char ch : line)
-			{
-				this->histogram[ch]++;
-			}
-		}
+		ReadCharactersFromInputFile();
 		break;
 	case 'd':
 		ReadHistogramFromInputFile();
 		break;
 	default:
-		return 1;
-	}
-
-	return 0;
-}
-
-void HuffmanCompression::PrintHistogram() const
-{
-	for (auto pair : histogram)
-	{
-		if (pair.second)
-		{
-			std::cout << pair.first << "\t" << pair.second << std::endl;
-		}
+		ErrorInvalidOption();
 	}
 }
 
-void HuffmanCompression::WriteHistogram()
-{
-	ResetToBeginningOutuputStream();
-
-	outStream << histogram.size() << std::endl;
-	for ( auto pair : histogram)
-	{
-		outStream << pair.first << pair.second << std::endl;
-	}
-}
-
-void HuffmanCompression::ReadHistogramFromInputFile()
-{
-	int size = 0;
-	char character;
-	UINT32 value = 0;
-
-	if (inStream.good())
-	{
-		inStream >> size;
-	}
-
-	while (inStream.good() && size > 0)
-	{
-		if (inStream.peek() == '\n')
-		{
-			inStream >> std::noskipws >> character;
-		}
-		inStream >> std::noskipws >> character;
-		if (inStream.good())
-		{
-			inStream >> std::noskipws >> value;
-			histogram[character] = value;
-		}
-		size--;
-	}
-}
-
-void HuffmanCompression::ResetToBeginningInputStream()
-{
-	this->inStream.clear();
-	this->inStream.seekg(0, std::ios_base::beg);
-}
-
-void HuffmanCompression::ResetToBeginningOutuputStream()
-{
-	this->outStream.clear();
-	this->outStream.seekp(0,std::ios_base::beg);
-}
-
-void HuffmanCompression::BuildHuffmanTree()
+void HuffmanCompression::BuildHuffmanTree(
+)
 {
 	std::priority_queue<HuffmanNode> nodes;
 	HuffmanNode *firstMaxNode, *secondMaxNode;
@@ -316,9 +306,9 @@ void HuffmanCompression::BuildHuffmanTree()
 
 		nodes.push(HuffmanNode(firstMaxNode, secondMaxNode));
 	}
-
+	
 	root = new HuffmanNode(nodes.top());
-
+	
 	nodes.pop();
 }
 
@@ -348,16 +338,18 @@ std::string HuffmanCompression::GenerateHuffmanCodeMap(
 	return code;
 }
 
-void HuffmanCompression::EncodeStream() 
+void HuffmanCompression::EncodeStream(
+) 
 {
-
 	std::string line;
-	uint32_t buffer[SIZE] = { 0 };
-	int index = 0;
+	UINTBITMAP buffer[SIZE] = { 0 };
+	UINTBITMAP index = 0;
+	
+	WriteHistogram();
+	#if DEBUG
 	
 	ResetToBeginningInputStream();
 
-	PrintCodeTable(huffmanCodeMap);
 	while (inStream.good())
 	{
 		std::getline(inStream, line);
@@ -369,35 +361,161 @@ void HuffmanCompression::EncodeStream()
 				UpdateBitInBuffer(buffer, index, bit - '0');
 				if (BufferFull(buffer, index))
 				{
+					#if DEBUG
+					std::cout << "Buffer is full.\nPrinting buffer...";
+					PrintBinaryBuffer(buffer, index);
+					std::cout << "DONE\nWriting buffer to file...\n";
+					#endif
+
 					WriteBuffer(buffer, index);
+					
+					#if DEBUG
+					std::cout << "DONE\nPrint buffer after writing to file...\n";
+					PrintBinaryBuffer(buffer, index);
+					#endif
 				}
 			}
 		}
 	}
 	WriteBuffer(buffer, index);
+	if (index)
+	{
+		outStream.seekp(0);
+		outStream << (UINTBITMAP)index;
+	}
+#endif
 }
 
-void HuffmanCompression::WriteBuffer(uint32_t list[], int& index)
+void HuffmanCompression::DecodeStream(
+)
 {
-	int bits = 32;
-	uint8_t mainIndex = 0;
-	uint8_t errorBits = 0;
+#if DEBUG
+	PrintHistogram(this->histogram);
+#endif
+}
+
+void HuffmanCompression::ResetToBeginningInputStream(
+)
+{
+	this->inStream.clear();
+	this->inStream.seekg(0, std::ios_base::beg);
+}
+
+void HuffmanCompression::ResetToBeginningOutuputStream(
+)
+{
+	this->outStream.clear();
+	this->outStream.seekp(0,std::ios_base::beg);
+}
+
+void HuffmanCompression::ReadCharactersFromInputFile(
+)
+{
+	std::string line;
+	while (inStream.good())
+	{
+		std::getline(inStream, line);
+		for (char ch : line)
+		{
+			this->histogram[ch]++;
+		}
+	}
 	
-	mainIndex = index / bits;
-	errorBits = index % bits;
+}
+
+void HuffmanCompression::ReadHistogramFromInputFile(
+)
+{
+	int size = 0;
+	char character;
+	UINTHISTVALUE value = 0;
+	UINTBITMAP errorBits = 0;
+
+	if (inStream.good())
+	{
+		inStream >> size;
+	}
+#if DEBUG
+	std::cout << "ReadHistogramFromInputFile:" << size << std::endl;
+	std::cout << "size = " << size << std::endl;
+#endif
+	while (inStream.good() && size > 0)
+	{
+		if (inStream.peek() == '\n')
+		{
+			inStream >> std::noskipws >> character;
+		}
+		inStream >> std::noskipws >> character;
+		if (inStream.good())
+		{
+			inStream >> std::noskipws >> value;
+			histogram[character] = value;
+		}
+		size--;
+	}
+#if DEBUG
+	PrintHistogram(this->histogram);
+#endif
+	HistogramEmptyCheck(this->histogram);
+
+}
+
+void HuffmanCompression::WriteHistogram(
+)
+{
+	ResetToBeginningOutuputStream();
+#if DEBUG
+	std::cout << "WriteHistogram: ";
+	std::cout << "histogram.size() = " << histogram.size() << std::endl;
+#endif
+	uint16_t 		count[1];
+	size_t 			i;
+	char 			*characters 	= new char[count[0]];
+	UINTHISTVALUE 	*value			= new UINTHISTVALUE[count[0]]; 
+	print	f("%d\n", count[0]);
+/*
+	for (auto pair : histogram)
+	{
+		if (i < count[1])
+		{
+			characters[i]	= pair.first;
+			value[i] 		= pair.second;
+			i++;
+		}
+	}
 	
+	for ( i = 0; i < count[1]; i++)
+	{
+		printf("%c\t%d\n", characters[i], value[i]);
+	}
+*/	
+	//write(outputFd, count, 1);
+	delete [] value;
+	delete [] characters;
+}
+
+void HuffmanCompression::WriteBuffer(
+	UINTBITMAP list[],
+	UINTBITMAP& index)
+{
+#if DEBUG
+	std::cout << "WriteStart\n";
+#endif
+	UINTBITMAP mainIndex = 0;
+	UINTBITMAP errorBits = 0;
+	
+	mainIndex = index / BITMAPENTRYBITS;
+	errorBits = index % BITMAPENTRYBITS;
+
+
 	for	(int i = 0; i <= mainIndex; i++)
 	{
-		outStream << list[i];
+		outStream << (UINTBITMAP)list[i];
 	}
+	
 	if (errorBits)
 	{
-		std::cout << errorBits << std::endl;
-		PrintBinaryBuffer(list, mainIndex + 1 );
-	}
-	if (errorBits)
-	{
-		uint32_t tmp = list[mainIndex + 1];
+		UINTBITMAP tmp = list[mainIndex];
 		ClearBuffer(list, SIZE);
 		list[0] = tmp;
 		index = errorBits;
@@ -407,7 +525,7 @@ void HuffmanCompression::WriteBuffer(uint32_t list[], int& index)
 		ClearBuffer(list, SIZE);
 		index = 0;
 	}
-	if (errorBits)
-	PrintBinaryBuffer(list, mainIndex + 1 );
-
+#if DEBUG
+	std::cout << "WriteEnd\n";
+#endif
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,535 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
 
-#include "HuffmanCompression.h"
+#include <queue>
+
+#define DEV 1
+#define DEBUG 0
+#define HISTSIZE 256
+
+struct HuffmanNode {
+	char character;
+	int  value;
+	int level;
+	const HuffmanNode *leftNode;
+	const HuffmanNode *rightNode;
+
+	HuffmanNode(char ch = '\0', int val = -1)
+	{
+		character = ch;
+		value = val;
+		leftNode = NULL;
+		rightNode = NULL;
+	}
+
+	HuffmanNode(const HuffmanNode *fNode, const HuffmanNode *sNode)
+	{
+		character = '\0';
+		value     = fNode->value + sNode->value;
+		leftNode  = fNode;
+		rightNode = sNode;
+	}
+
+	bool operator< (const HuffmanNode &node) const
+	{
+		return value > node.value;
+	}
+};
+
+static char option;
+static std::string pathToInputFile;
+static std::string pathToOutputFile;
+
+static std::ifstream inputStream;
+static std::ofstream outputStream;
+
+static uint16_t histogram[HISTSIZE];
+static uint8_t	hist_size;
+static uint8_t 	error_bits;
+static std::string huffmanCodeMap[HISTSIZE];
+
+struct HuffmanNode *root = NULL;
+
+void ProcessInput(
+	int argc,
+	char *argv[]);
+
+void OpenWorkFiles();
+
+void CloseWorkFiles();
+
+void GenerateHistogram();
+
+void WriteHistogram();
+
+void PrintDebugHistogram()
+{
+	printf("Listing histogram\n");
+	for (uint16_t i = 0; i < HISTSIZE; i++)
+	{
+		if(histogram[(uint8_t)i])
+		{
+			printf("\t%c\t=\t%d\n", (uint8_t)i, histogram[(uint8_t)i]);		
+		}
+	}
+	printf("End listing\n");
+	
+}
+
+void PrintDebugCodeMap()
+{
+	printf("Listing code map\n");
+	for (uint16_t i = 0; i < HISTSIZE; i++)
+	{
+		if(histogram[i])
+		{	
+			printf("\t%c\t=\t%s\n", (char)i, huffmanCodeMap[i].c_str());
+		}
+	}
+	printf("End listing\n");
+}
+void BuildHuffmanTree();
+
+std::string GenerateHuffmanCodeMap(
+	const HuffmanNode *root,
+	std::string code);
+
+void EncodeStream();
+
+void DecodeStream();
+
+std::string HuffmanCompressionGenerateHuffmanCodeMap(
+	const HuffmanNode *root,
+	std::string code);
+
 int main(
 	int argc,
 	char *argv[])
 {
-	HuffmanCompression compression;
-	compression.Run(argc, argv);
+	ProcessInput(argc, argv);
+	OpenWorkFiles();
 
+	GenerateHistogram();
+	BuildHuffmanTree();
+	GenerateHuffmanCodeMap(root, "");
+	PrintDebugCodeMap();
+
+	// By this point everything is set. We can compess or decompress now.
+	if (option == 'c')
+	{
+		EncodeStream();
+	}
+	else
+	{
+		DecodeStream();
+	}
+
+	CloseWorkFiles();
 	return 0;
+}
 
+static void ErrorInvalidInputFormat()
+{
+	std::cerr << "error: Invalid input format.\nusage:\tcompressSoft -[c/d] -[i/o] fileName -[i/o] fileName\n";
+	std::cerr << "c : compress\td : decmpress\ni : input\to : output\n";
+	exit(1);
+}
+
+static void ErrorInvalidOption(
+)
+{
+	std::cerr << "error: Invalid option. Only -c or -d.";
+	exit(1);
+}
+
+static void ErrorOpenFile(
+	std::string pathToInputFile)
+{
+	std::cerr << "error: failed to open file '" << pathToInputFile << "'\n";
+	exit(1);
+}
+
+static void HistogramEmptyCheck()
+{
+	if (hist_size == 0)
+	{
+		std::cerr << "error:\tNo histogram available. Please check your input file." << std::endl;
+		exit(1);
+	}
+}
+
+void ProcessInput(
+	int argc,
+	char *argv[])
+{
+	/* Check argument length. */
+	
+	/* Check individual arguments lenght. */
+	if (argc 			!= 6 ||
+		strlen(argv[1]) != 2 ||
+	    strlen(argv[2]) != 2 ||
+	    strlen(argv[4]) != 2)
+	 {
+	 	ErrorInvalidInputFormat();
+	 }
+
+	/* First option for -[c/d] */
+	if (argv[1][0] != '-' ||
+	   (argv[1][1] != 'c' &&
+	    argv[1][1] != 'd'))
+	 {
+		 ErrorInvalidOption();
+	 }
+
+	/* Second and third option for -[i/o] */
+	if (argv[2][0] != '-'  ||
+	   (argv[2][1] != 'i'  &&
+	    argv[2][1] != 'o') ||
+	   (argv[4][1] != 'i'  &&
+	    argv[4][1] != 'o') ||
+	   (argv[2][1] == 'i'  &&
+	    argv[4][1] == 'i') ||
+	   (argv[2][1] == 'o'  &&
+	    argv[4][1] == 'o'))
+	 {
+		ErrorInvalidInputFormat();
+	 }
+
+	int indexToInputFile  = (argv[2][1] == 'i')? 3 : 5;
+	int indexToOutputFile = (argv[2][1] == 'o')? 3 : 5;
+
+	option = argv[1][1];
+	pathToInputFile = argv[indexToInputFile];
+	pathToOutputFile = argv[indexToOutputFile];
+}
+
+void OpenWorkFiles()
+{
+	inputStream.open( pathToInputFile.c_str(), std::ios::in | std::ios_base::binary | std::ios::ate);
+	if (!inputStream)
+	{
+		ErrorOpenFile(pathToInputFile);
+	}
+
+	outputStream.open( pathToOutputFile.c_str(), std::ios::out | std::ios_base::binary);
+	if (!outputStream)
+	{
+		ErrorOpenFile(pathToOutputFile);
+	}
+}
+
+void CloseWorkFiles()
+{
+	inputStream.close();
+	outputStream.close();
+}
+
+void ReadCharactersFromInputFile()
+{
+	std::streampos size;
+	char buffer[HISTSIZE];
+	if (inputStream.is_open())
+	{
+    size = inputStream.tellg();
+    char *memblock = new char [size];
+    inputStream.seekg (0, std::ios::beg);
+    inputStream.read (memblock, size);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		histogram[memblock[i]]++;
+	}
+	for (size_t i = 0; i < HISTSIZE; i++)
+	{
+		if(histogram[i])
+		{
+			hist_size++;
+		}
+	}
+    delete[] memblock;
+  }
+#if DEBUG
+	PrintDebugHistogram();
+#endif
+}
+
+//TODO: Fine tune a little bit. Or yes.
+void ReadHistogramFromInputFile()
+{
+#if DEBUG
+	printf("Reading histogram from input file\n");
+#endif
+	if (inputStream.is_open())
+	{
+	    char buffer[1] = {' '};
+		inputStream.seekg(0, std::ios_base::beg);
+		inputStream.read(buffer, 1);
+		error_bits = buffer[0];
+		inputStream.read(buffer, 1);
+		hist_size = buffer[0];
+	}
+	if (inputStream.is_open() && hist_size > 0)
+	{
+		char buffer[2*hist_size] = { 0 };
+		inputStream.read(buffer, 2*hist_size);
+		for (size_t i = 0; i < 2 * hist_size; i+=2)
+		{
+			histogram[buffer[i]] = buffer[i+1]; 
+		}
+#if DEBUG
+	PrintDebugHistogram();
+#endif
+	}
+#if DEBUG
+	printf("End reading\n");
+#endif
+}
+
+void GenerateHistogram()
+{
+	//TODO: Depending on the option value, the histogram will be generated
+	//		by a text for encoding or a from an encoded file.
+	//		In one way it is straightforward. The other needs more attention.
+	//DONE!
+	switch (option)
+	{
+	case 'c':
+		ReadCharactersFromInputFile();
+		break;
+	case 'd':
+		ReadHistogramFromInputFile();
+		break;
+	default:
+		ErrorInvalidOption();
+	}
+}
+
+//TODO: Tune a little bit more. Or not.
+void WriteHistogram()
+{
+#if DEBUG
+	std::cout << "Write histogram\n";
+#endif
+
+	char buffer[2 * hist_size] = { 0 };
+	buffer[0] = hist_size;
+#if DEBUG
+	printf("\thist_size = %d\n", (int)hist_size);
+	printf("\tbuffer[0] = %d\n", buffer[0]);
+#endif
+
+	//outputStream.seekp(0, std::ios_base::beg);
+	outputStream.write(buffer, 1);
+	for (uint16_t i = 0, j = 0; i < HISTSIZE && j < 2 * hist_size; i++)
+	{
+		if(histogram[i])
+		{
+			buffer[j] = i;
+			j++;
+			buffer[j] = histogram[i];
+			j++;		
+		}
+	}
+
+	outputStream.write(buffer, 2 * hist_size);
+#if DEBUG
+	printf("Listing buffer histogram\n");
+	for (size_t i = 0; i < 2 * hist_size; i+=2)
+	{
+		printf("\t%c = %d\n", (char)buffer[i], (int)buffer[i+1]);
+	}
+	
+	std::cout << "End writing\n";
+#endif
+}
+
+void BuildHuffmanTree()
+{
+	std::priority_queue<HuffmanNode> nodes;
+	HuffmanNode *firstMaxNode, *secondMaxNode;
+	for (uint16_t i = 0; i < HISTSIZE; i++)
+	{
+		if (histogram[i])
+		{
+			nodes.push(HuffmanNode(i, histogram[i]));
+		}
+	}
+
+	while (nodes.size() > 1)
+	{
+		firstMaxNode = new HuffmanNode(nodes.top());
+		nodes.pop();
+
+		secondMaxNode = new HuffmanNode(nodes.top());
+		nodes.pop();
+
+		nodes.push(HuffmanNode(firstMaxNode, secondMaxNode));
+	}
+	
+	root = new HuffmanNode(nodes.top());
+	
+	nodes.pop();
+}
+
+std::string GenerateHuffmanCodeMap(
+	const HuffmanNode *root,
+	std::string code)
+{
+	if (root == NULL)
+	{
+		return "";
+	}
+
+	if (root->leftNode)
+	{
+		GenerateHuffmanCodeMap(root->leftNode, code + "0");
+	}
+	if (root->rightNode)
+	{
+		GenerateHuffmanCodeMap(root->rightNode, code + "1");
+	}
+
+	if (root->leftNode == NULL &&
+	    root->rightNode == NULL)
+	{
+		huffmanCodeMap[root->character] = code;
+	}
+	return code;
+}
+
+static int UpdateBitInBuffer(
+	char list[],
+	int bits,
+	int& index,
+	int bitValue)
+{
+	if (index < 0 || index >= bits * HISTSIZE)
+	{
+		return 1;
+	}
+
+
+	int minorIndex = index % bits;
+	int mainIndex = index / bits;
+
+	if (!!bitValue)
+	{
+		list[mainIndex] = list[mainIndex] | (1 << (bits - minorIndex - 1));
+	}
+	else
+	{
+		list[mainIndex] = list[mainIndex] & ~(1 << (bits - minorIndex - 1));
+	}
+
+	index++;
+	return 0;
+}
+
+static int BufferFull(
+	int bits,
+	int& index)
+{
+	if (index >= (bits * HISTSIZE))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+//TODO: Error  bit not handled.
+void EncodeStream() 
+{
+	std::string line;
+	char buffer[HISTSIZE] = { 0 };
+	int index = 0;
+
+	inputStream.seekg(0, std::ios_base::beg);
+	outputStream.write(buffer, 1);
+	WriteHistogram();
+
+	while (inputStream.good())
+	{
+		std::getline(inputStream, line);
+
+		for (char ch : line)
+		{
+			for (char bit : huffmanCodeMap[ch])
+			{
+				std::cout << bit;
+				UpdateBitInBuffer(buffer, 8, index, bit - '0');
+				if (BufferFull(8, index))
+				{
+					outputStream.write(buffer, HISTSIZE);
+					index = 0;
+				}
+			}
+		}
+	}
+
+	if (index)
+	{
+		int minorIndex = index % 8;
+		int mainIndex = index / 8;
+		printf("\nminorIndex = %d\nmainIndex = %d\nindex = %d\n", minorIndex, mainIndex, index);
+		outputStream.write(buffer, mainIndex);
+		if (minorIndex)
+		{
+			outputStream.write(buffer, 1);
+			outputStream.seekp(0, std::ios_base::beg);
+			buffer[0] = minorIndex;
+			outputStream.write(buffer, 1);
+		}
+	}
+}
+
+void DecodeStream()
+{
+	std::streampos size, pos;
+	if (inputStream.good())
+	{
+		inputStream.seekg(0, std::ios_base::end);
+		size = inputStream.tellg();
+		pos = 2 * hist_size + 2;
+		inputStream.seekg(pos);
+
+		char buffer[HISTSIZE] = { 0 };
+
+		inputStream.read(buffer, size - pos);
+		const struct HuffmanNode *ptr = root;
+		int index = 0;
+		for(int i = 0; i < size - pos; i++)
+		{
+			uint8_t mask = 1 << 8 - 1;
+			while (mask)
+			{
+				if((buffer[i] & mask) != 0)
+				{
+					if (ptr->rightNode == NULL)
+					{
+						std::cout << ptr->character;
+						ptr = root->rightNode;
+					}
+					else
+					{
+						ptr = ptr->rightNode;
+					}
+				}
+				else
+				{
+					if (ptr->leftNode == NULL && ptr->rightNode == NULL)
+					{
+						std::cout << ptr->character;
+						ptr = root->leftNode;
+					}
+					else
+					{
+						ptr = ptr->leftNode;
+					}
+				}
+				index++;
+				mask >>= 1;
+			}
+		}	
+	}
 }
